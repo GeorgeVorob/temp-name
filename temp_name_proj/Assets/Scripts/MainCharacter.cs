bba2a3d2dda1e774688b10798947b7f47e5a24e9
AniskovAlex <54923203+AniskovAlex@users.noreply.github.com>
2020-08-03 00:21:37 +0700

    Добавление Главного Героя и контроллера перелвижения
    Добавление палитры из объктов пола
    Добавление объекта игрока
    Добавление контроллера игрока, который осуществляет перемещение по оси х и прыжок

---
+++ temp_name_proj/Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainCharacter : MonoBehaviour
+{
+    Rigidbody2D body;
+    float horizontal;// Переменная перемещения по оси х
+    public float speed;// Переменная скорости перемещения
+    public bool grounded=false;// Переиенная состояния "на земле"
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal= Input.GetAxis("Horizontal"); 
+        /* Обработка нажатия кнопки прыжка */
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            body.AddForce(new Vector2(0f, 500f)); // -В чём сила брат? -В ньютанах. 
+            grounded = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = body.position;
+        Vector2 move = new Vector2(horizontal, 0);
+        position += move * speed * Time.deltaTime;
+        body.position = position;
+    }
+
+}
