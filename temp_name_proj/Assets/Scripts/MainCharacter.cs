cb760c3cab5742a6b5ee7517e471f75fb0699901
GeorgeVorob <george77019721@gmail.com>
2020-08-09 22:47:28 +0700

    Перенос телекинеза в класс
    Также переименован материал персонажа
    Также телекинез теперь обрабатывается в FixedUpdate, так что его сила сильно увеличена

--- temp_name_proj/Assets/Scripts/MainCharacter.cs
+++ temp_name_proj/Assets/Scripts/MainCharacter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,14 +12,15 @@ public class MainCharacter : MonoBehaviour
     public float force;
     public bool grounded=false;// Переиенная состояния "на земле"
 
-    Vector3 prevForce = new Vector3(0f, 0f, 0f);
-    private bool grabbing = false;
+    Telekinesis telekines = new Telekinesis(); //объект телекинеза
+
     GameObject lastClicked; //последний GameObject, на который нажал игрок (при отключенном захвате)
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        telekines.avalible = true;
        // RigidbodyInterpolation2D a = new RigidbodyInterpolation2D();
        // var a =body.interpolation;
     }
@@ -32,40 +34,29 @@ public class MainCharacter : MonoBehaviour
             body.AddForceAtPosition(new Vector2(0f, force), body.position);
             grounded = false;
         }
-        horizontal= Input.GetAxis("Horizontal"); 
+        horizontal= Input.GetAxis("Horizontal");
         /* Обработка нажатия кнопки прыжка */
-
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (grabbing)
-            {
-                grabbing = false;
-            }
-            else
-            {
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); 
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
-                if ((hit.collider != null) && (grabbing == false))
+            if ((hit.collider != null))
+            {
+                lastClicked = hit.collider.gameObject;
+            }
+
+            if (telekines.avalible)
+            {
+                if(!telekines.working)
                 {
-                    lastClicked = hit.collider.gameObject;
-                    grabbing = true;
+                    telekines.Start(lastClicked.GetComponent<Rigidbody2D>());
+                }
+                else
+                {
+                    telekines.Stop();
                 }
             }
-        }
-        if(grabbing) //полет Rigidbody2D за курсором
-        {
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //позиция курсора
-            var mouseDir = mousePos - lastClicked.transform.position; //направление от объекта до курсора
-            mouseDir.z = 0.0f; //TODO: перевести этот ужас в Vector2
-            if (mouseDir.magnitude > 5f) //если расстояние между курсором и объектом слишком велико (а вместе с ним и скорость объекта, то всё сбрасывается
-            {
-                grabbing = false;
-                lastClicked.GetComponent<Rigidbody2D>().gravityScale = 1;
-                prevForce.Set(0f, 0f, 0f);
-            }
-            lastClicked.GetComponent<Rigidbody2D>().AddForce((mouseDir.normalized * 3000f * mouseDir.magnitude) - prevForce * 0.92f); //собственно, движение объекта
-            prevForce = mouseDir.normalized * 3000f * mouseDir.magnitude;
         }
 
     }
@@ -78,7 +69,11 @@ public class MainCharacter : MonoBehaviour
         //position.x += horizontal * speed * Time.deltaTime;
         // body.position = position;
         body.velocity = new Vector2(horizontal*speed, body.velocity.y);
-        Debug.Log(body.velocity);
+       // Debug.Log(body.velocity);
+        if(telekines.working)
+        {
+            telekines.Work();
+        }
         
     }
 
