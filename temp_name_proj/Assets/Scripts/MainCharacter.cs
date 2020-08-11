419e70b42b309869c8ba8e3eb61b57a709e0b3b2
AniskovAlex <54923203+AniskovAlex@users.noreply.github.com>
2020-08-11 20:44:54 +0700

    Передвижение: взбирание
    взбирание через насильное изменение положение персонажа

--- temp_name_proj/Assets/Scripts/MainCharacter.cs
+++ temp_name_proj/Assets/Scripts/MainCharacter.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class MainCharacter : MonoBehaviour
@@ -11,14 +13,15 @@ public class MainCharacter : MonoBehaviour
     public float force;
     public bool grounded=false;// Переиенная состояния "на земле"
 
-    Vector3 prevForce = new Vector3(0f, 0f, 0f);
-    private bool grabbing = false;
+    Telekinesis telekines = new Telekinesis(); //объект телекинеза
+
     GameObject lastClicked; //последний GameObject, на который нажал игрок (при отключенном захвате)
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        telekines.avalible = true;
        // RigidbodyInterpolation2D a = new RigidbodyInterpolation2D();
        // var a =body.interpolation;
     }
@@ -26,60 +29,90 @@ public class MainCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        RaycastHit2D leftFeet = Physics2D.Raycast(body.position+new Vector2(-0.5f,-0.9f),Vector2.down,0.05f);
+        RaycastHit2D RightFeet = Physics2D.Raycast(body.position + new Vector2(0.5f, -0.9f), Vector2.down, 0.05f);
+        if (leftFeet.collider != null || RightFeet.collider != null)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
         if (Input.GetButtonDown("Jump") && grounded)
         {
-            //body.AddForce(); // -В чём сила брат? -В ньютанах. 
+            //body.AddForce(); // -В чём сила брат? -В ньютанах.
             body.AddForceAtPosition(new Vector2(0f, force), body.position);
             grounded = false;
         }
-        horizontal= Input.GetAxis("Horizontal"); 
-        /* Обработка нажатия кнопки прыжка */
+        horizontal= Input.GetAxis("Horizontal");
 
+        if (horizontal > 0)
+        {
+            RaycastHit2D climpBottom = Physics2D.Raycast(body.position + new Vector2(0.55f, -0.9f), Vector2.right, 0.1f);
+            RaycastHit2D climptop = Physics2D.Raycast(body.position + new Vector2(0.55f, -0.7f), Vector2.right, 0.1f);
+            if (climpBottom.collider != null && climptop.collider == null)
+            {
+                body.transform.position += (Vector3)new Vector2(0.1f, 0.2f);
+                body.velocity = new Vector2(body.velocity.x, 0);
+            }
+        }
+
+        if (horizontal < 0)
+        {
+            RaycastHit2D climpBottom = Physics2D.Raycast(body.position + new Vector2(-0.55f, -0.9f), Vector2.right, 0.1f);
+            RaycastHit2D climptop = Physics2D.Raycast(body.position + new Vector2(-0.55f, -0.7f), Vector2.right, 0.1f);
+            if (climpBottom.collider != null && climptop.collider == null)
+            {
+                body.transform.position += (Vector3)new Vector2(-0.1f, 0.2f);
+                body.velocity = new Vector2(body.velocity.x, 0);
+            }
+        }
+
+        /*if (horizontal < 0)
+        {
+            RaycastHit2D climpBottom = Physics2D.Raycast(body.position + new Vector2(-0.55f, -0.9f), Vector2.left, 0.1f);
+            RaycastHit2D climptop = Physics2D.Raycast(body.position + new Vector2(-0.55f, -0.6f), Vector2.left, 0.1f);
+            if (climpBottom.collider != null && climptop.collider == null) body.AddForce(new Vector2(0, 900f));
+        }*/
+
+        /* Обработка нажатия кнопки прыжка */
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (grabbing)
-            {
-                grabbing = false;
-            }
-            else
-            {
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); 
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
-                if ((hit.collider != null) && (grabbing == false))
+            if ((hit.collider != null))
+            {
+                lastClicked = hit.collider.gameObject;
+            }
+
+            if (telekines.avalible)
+            {
+                if(!telekines.working)
                 {
-                    lastClicked = hit.collider.gameObject;
-                    grabbing = true;
+                    telekines.Start(lastClicked.GetComponent<Rigidbody2D>());
+                }
+                else
+                {
+                    telekines.Stop();
                 }
             }
-        }
-        if(grabbing) //полет Rigidbody2D за курсором
-        {
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //позиция курсора
-            var mouseDir = mousePos - lastClicked.transform.position; //направление от объекта до курсора
-            mouseDir.z = 0.0f; //TODO: перевести этот ужас в Vector2
-            if (mouseDir.magnitude > 5f) //если расстояние между курсором и объектом слишком велико (а вместе с ним и скорость объекта, то всё сбрасывается
-            {
-                grabbing = false;
-                lastClicked.GetComponent<Rigidbody2D>().gravityScale = 1;
-                prevForce.Set(0f, 0f, 0f);
-            }
-            lastClicked.GetComponent<Rigidbody2D>().AddForce((mouseDir.normalized * 3000f * mouseDir.magnitude) - prevForce * 0.92f); //собственно, движение объекта
-            prevForce = mouseDir.normalized * 3000f * mouseDir.magnitude;
         }
 
     }
 
     void FixedUpdate()
     {
-        
-        Vector2 position = body.position;
-        //Vector2 move = new Vector2(, 0);
-        //position.x += horizontal * speed * Time.deltaTime;
-        // body.position = position;
         body.velocity = new Vector2(horizontal*speed, body.velocity.y);
-        Debug.Log(body.velocity);
-        
+       // Debug.Log(body.velocity);
+        if(telekines.working)
+        {
+            telekines.Work();
+        }
+
     }
 
 }
---
+++ temp_name_proj/Assets/Scripts/Telekinesis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Telekinesis
+    {
+        private Rigidbody2D body;
+        private Vector3 prevForce = new Vector3(0f, 0f, 0f);
+        public Boolean working { get; set; }
+        public Boolean avalible { get; set; }
+        public Telekinesis()
+        {
+            working = false;
+            avalible = false;
+        }
+        public void Start(Rigidbody2D body)
+        {
+            working = true;
+            this.body = body;
+        }
+        public void Work()
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //позиция курсора
+            var mouseDir = mousePos - body.transform.position; //направление от объекта до курсора
+            mouseDir.z = 0.0f; //TODO: перевести этот ужас в Vector2
+            if (mouseDir.magnitude > 5f) //если расстояние между курсором и объектом слишком велико (а вместе с ним и скорость объекта, то всё сбрасывается
+            {
+                working = false;
+                body.gravityScale = 1;
+                prevForce.Set(0f, 0f, 0f);
+            }
+            body.AddForce((mouseDir.normalized * 20000f * mouseDir.magnitude) - prevForce * 0.92f); //собственно, движение объекта
+            prevForce = mouseDir.normalized * 20000f * mouseDir.magnitude;
+            Debug.Log(prevForce);
+        }
+        public void Stop()
+        {
+            working = false;
+        }
+    }
+}
